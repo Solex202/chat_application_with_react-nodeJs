859bc6e68f42aaba17885addc258a7eed06afb11
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-02-09 22:36:20 +0100

    implementing chat app code

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Routes, Route, BrowserRouter} from 'react-router-dom';
 
-import Join from './components/Join';
-import Chat from './components/Chat';
+import Join from './components/Join/Join';
+import Chat from './components/Chat/Chat';
 
 const App = () => (
     <BrowserRouter>
